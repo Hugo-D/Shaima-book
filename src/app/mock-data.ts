b3f46bc45e5cc26697df673e7c75e4d6007b15f0
Denis Hugo <hugo.denis@etudiant.univ-rennes1.fr>
2022-05-16 21:30:31 +0200

    Detail et exemple des data images Tentative de deploiement sur GitHub

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -1,5 +1,7 @@
 import { IImage } from "./entities/IImage";
 
 export const IMAGE: IImage[] = [
-    {nom: "image-accueil", tag: "accueil", description: "", lien: "https://drscdn.500px.org/photo/101577319/m%3D900/v2?sig=3972e8661c4c8d8df25699cbf8e6d3aaac5aa5bb4b2e11d661e16c3de42d3c72"}
+    {nom: "image-accueil", tag: "accueil", description: "", lien: "https://drscdn.500px.org/photo/101577319/m%3D900/v2?sig=3972e8661c4c8d8df25699cbf8e6d3aaac5aa5bb4b2e11d661e16c3de42d3c72"},
+    {nom: "Titre de ma 1re création", tag: "portfoliot", description: "Une description de ma 1re création. <b>ça c'est en gras</b>, et ça, <i>en italique</i>, tu peux <b><i>combiner les deux</i></b>. Et les retours à la ligne, c'est comme ça :<br>Hope je suis à la ligne !<br>Au fait, pour les guillemets doubles, faut rajouter un anti-slash devant : \"entre guillemets\" voiliiii ^^", lien: "une url vers l'image de ta création, si tu as déposé l'image sur Google Drive, accessible par lien en public, ça devrait le faire !"}
+
 ]
